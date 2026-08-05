a83c8debc9f358a593f96fde548a4edacf001399
Neva <nevo.krien@gmail.com>
2026-08-05 23:40:47 +0300

    more readable macros

--- sys-tester/src/seccomp_filters.rs
+++ sys-tester/src/seccomp_filters.rs
@@ -3,11 +3,22 @@ compile_error!("seccomp filters currently support only x86_64");
 
 use std::mem::size_of;
 
+macro_rules! generated_filter_bytes {
+    ($file:literal) => {
+        include_bytes!(concat!(env!("OUT_DIR"), "/", $file))
+    };
+}
+
+macro_rules! filter_len {
+    ($file:literal) => {
+        generated_filter_bytes!($file).len() / size_of::<libc::sock_filter>()
+    };
+}
+
 macro_rules! include_filter {
     ($file:literal) => {{
-        const BYTES: &[u8; include_bytes!(concat!(env!("OUT_DIR"), "/", $file)).len()] =
-            include_bytes!(concat!(env!("OUT_DIR"), "/", $file));
-        const LEN: usize = BYTES.len() / size_of::<libc::sock_filter>();
+        const BYTES: &[u8; generated_filter_bytes!($file).len()] = generated_filter_bytes!($file);
+        const LEN: usize = filter_len!($file);
         const { assert!(BYTES.len() % size_of::<libc::sock_filter>() == 0) };
 
         unsafe { std::mem::transmute::<[u8; BYTES.len()], [libc::sock_filter; LEN]>(*BYTES) }
@@ -16,11 +27,11 @@ macro_rules! include_filter {
 
 const _: () = assert!(size_of::<libc::sock_filter>() == 8);
 
-#[allow(dead_code)]
-pub const X86_64_BOOTSTRAP: [libc::sock_filter;
-    include_bytes!(concat!(env!("OUT_DIR"), "/x86_64_bootstrap.bpf")).len()
-        / size_of::<libc::sock_filter>()] = include_filter!("x86_64_bootstrap.bpf");
+type SeccompFilter<const LEN: usize> = [libc::sock_filter; LEN];
 
-pub const X86_64_FINAL: [libc::sock_filter;
-    include_bytes!(concat!(env!("OUT_DIR"), "/x86_64_final.bpf")).len()
-        / size_of::<libc::sock_filter>()] = include_filter!("x86_64_final.bpf");
+#[allow(dead_code)]
+pub const X86_64_BOOTSTRAP: SeccompFilter<{ filter_len!("x86_64_bootstrap.bpf") }> =
+    include_filter!("x86_64_bootstrap.bpf");
+
+pub const X86_64_FINAL: SeccompFilter<{ filter_len!("x86_64_final.bpf") }> =
+    include_filter!("x86_64_final.bpf");
